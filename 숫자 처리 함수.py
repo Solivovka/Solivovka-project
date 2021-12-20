f1c7ec774d6d674b4c47b5953536cdbae79b621d
Jemin Lee <po9195@naver.com>
2021-12-21 00:08:01 +0900

    first commit

---
+++ 숫자 처리 함수.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 5 , abs = absolute value = 절댓값 
+print(pow( 4 , 2 )) # 4^2 = 4*4 = 16 , pow = 거듭제곱함수 
+print(max(5 , 12)) # 12 , max = Maximum value = 최댓값
+print(min( 5 , 12 )) # 5 , Minimum value = 최솟값 
+print(round( 3.14 )) # 3 , round = 반올림
+print(round( 4.99 )) # 5 , round = 반올림
+
+from math import *
+print(floor(4.99)) # 내림, 4
+print(ceil( 3.14 )) # 올림, 4
+print(sqrt( 16 )) # 제곱근, 4 , square root = 제곱근
